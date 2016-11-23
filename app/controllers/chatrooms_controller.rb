94b3aefef0df351163335cdca58ff05eac0617ca
marat <marat.chardymov@gmail.com>
2016-11-24 00:14:41 +0300

    action cable init

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,6 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find_by(topic: params[:topic])
-    byebug
     @message = Message.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chatrooms, param: :topic
   resources :messages
